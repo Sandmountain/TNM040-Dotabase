8ef932479da9c12311b9a6562abe74f6a08cab30
Isprinsessan <eriknilsssonn@gmail.com>
2016-11-01 15:56:11 +0100

    Heroes.js uppdaterad

--- script/heroes.js
+++ script/heroes.js
@@ -1,12 +1,12 @@
-  $(function(){
+function GetHero(data) {
 
     var heroes = [];
 
     $.getJSON(../json/heroes.json, function(data){
-      $.eah(data.heroes,function(i, f){
+      $.each(data.heroes,function(i, f){
       	var tblRow = "<tr>" + "<td>" + f.localizedName + "</td>" +
       	"<td>" + f.id + "</td>" + "<td>" + f.abilities + "</td>" + "</tr>"
       	$(tblRow).appendTo("#heroesdata tbody");
       })
     });
-  });
+}
