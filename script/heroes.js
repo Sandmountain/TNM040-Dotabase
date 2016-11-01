292fbdb64e65d280f3fe9d19a0fb343e0e7b6b04
Isprinsessan <eriknilsssonn@gmail.com>
2016-11-01 15:38:18 +0100

    asd

--- script/heroes.js
+++ script/heroes.js
@@ -2,7 +2,11 @@
 
     var heroes = [];
 
-    $.getJSON(heroes.json, function(data){
-      //do shit
+    $.getJSON(../json/heroes.json, function(data){
+      $.eah(data.heroes,function(i, f){
+      	var tblRow = "<tr>" + "<td>" + f.localizedName + "</td>" +
+      	"<td>" + f.id + "</td>" + "<td>" + f.abilities + "</td>" + "</tr>"
+      	$(tblRow).appendTo("#heroesdata tbody");
+      })
     });
   });
